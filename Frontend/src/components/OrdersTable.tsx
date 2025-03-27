4750968e3b90f05d3af7679824bab1cec5c31219
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2025-03-27 16:50:30 -0300

    update [OrderTable]: adds button to create Order and import OrderForm componet

--- Frontend/src/components/OrdersTable.tsx
+++ Frontend/src/components/OrdersTable.tsx
@@ -1,26 +1,64 @@
-import { Link } from "react-router-dom";
 import { IOrder } from "../types/order.interface";
+import { JSX, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createOrder } from "../services/orderService";
+import OrderForm from "./OrderForm";
 
 interface OrdersTableProps {
   orders: IOrder[];
   loader: boolean;
+  setOrders: (orders: IOrder[]) => void; 
 }
 
 /**
  * Componente de tabela de pedidos
  *
  * @param {IOrder[]} orders - Lista de pedidos
+ * @param {Function} setOrders - Função para atualizar pedidos
  * @param {boolean} loader - Indicador de carregamento da tabela
  * @returns {JSX.Element} - Retorna a tabela dos pedidos
  */
-export function OrdersTable({ orders, loader }: OrdersTableProps) {
+export function OrdersTable({
+  orders,
+  setOrders,
+  loader,
+}: OrdersTableProps): JSX.Element {
+  const navigate = useNavigate();
+  const [enableNewOrder, setEnableNewOrder] = useState<boolean>(false);
+
+  const goToOrderDetail = (orderid: string) => {
+    navigate(`/order/${orderid}`);
+  };
+
+  const onSubmit = async (newOrder: Partial<IOrder>) => {
+    const order = await createOrder(newOrder);
+
+    if (order) {
+      setOrders([...orders, order]);
+    }
+  };
+
   return (
     <div>
+      <button
+        className="flex items-center px-4 py-1 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer mb-4"
+        onClick={() => setEnableNewOrder(!enableNewOrder)}
+      >
+        Novo Pedido
+        <span className="ml-2 text-xl">+</span>
+      </button>
+
+      {enableNewOrder && (
+        <div className="mb-6">
+          <OrderForm onSubmit={onSubmit} />
+        </div>
+      )}
+
       {loader ? (
         <div>Carregando...</div>
       ) : (
         <>
-          <h2 className="text-xl font-bold mb-2">Lista de Pedidos</h2>
+          <h3 className="font-semibold mb-2">Lista de Pedidos</h3>
           <table className="w-full border-collapse border border-gray-200">
             <thead>
               <tr className="bg-gray-200">
@@ -34,14 +72,14 @@ export function OrdersTable({ orders, loader }: OrdersTableProps) {
             </thead>
             <tbody>
               {orders.map((order) => (
-                <tr key={order.id} className="hover:bg-gray-50">
+                <tr
+                  key={order.id}
+                  className="hover:bg-gray-50 cursor-pointer"
+                  onClick={() => goToOrderDetail(order.id)}
+                >
                   <td className="border p-2">{order.id}</td>
                   <td className="border p-2">{order.cliente}</td>
-                  <td className="border p-2">
-                    <Link className="text-blue-500" to={`/order/${order.id}`}>
-                      {order.produto}
-                    </Link>
-                  </td>
+                  <td className="border p-2">{order.produto}</td>
                   <td className="border p-2">R$ {order.valor}</td>
                   <td className="border p-2">{order.status}</td>
                   <td className="border p-2">
