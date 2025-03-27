9c62d33f7bf3bea44e8ffccd2cb1fb78832dda0d
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2025-03-27 16:48:58 -0300

    feat [Service]: creates endpoint to create Order

--- Frontend/src/services/orderService.ts
+++ Frontend/src/services/orderService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { IOrder } from "../types/order.interface";
 
-const API_URL = "http://localhost:5222/api/Order"; 
+const API_URL = "http://localhost:5222/api/Order";
 
 export const getOrders = async (): Promise<IOrder[]> => {
   try {
@@ -10,5 +10,17 @@ export const getOrders = async (): Promise<IOrder[]> => {
   } catch (error) {
     console.error("Erro ao buscar pedidos", error);
     return [];
+  }
+};
+
+export const createOrder = async (
+  newOrder: Partial<IOrder>
+): Promise<IOrder | null> => {
+  try {
+    const response = await axios.post<IOrder>(API_URL, newOrder);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao criar pedido", error);
+    return null;
   }
 };
