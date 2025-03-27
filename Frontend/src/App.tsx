01b63506e6fb3a66ff09ca3bc6848123319e6e45
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2025-03-27 16:51:28 -0300

    update [App]: refactor code, removes unused navbar and adds new Route for OrderDetail component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { OrdersTable } from "./components/OrdersTable";
 import { IOrder } from "./types/order.interface";
 import { getOrders } from "./services/orderService";
+import { OrderDetail } from "./components/OrderDetail";
 
 export default function App() {
   const [orders, setOrders] = useState<IOrder[]>([]);
-
   const [loader, setLoader] = useState<boolean>(false);
 
   useEffect(() => {
@@ -21,21 +21,29 @@ export default function App() {
 
   return (
     <Router>
-      <div className="p-4">
-        <nav className="mb-4">
-          <Link className="mr-4 text-blue-500" to="/">
-            Pedidos
-          </Link>
-          <Link className="text-blue-500" to="/new">
-            Novo Pedido
-          </Link>
-        </nav>
-        <Routes>
-          <Route
-            path="/"
-            element={<OrdersTable orders={orders} loader={loader} />}
-          />
-        </Routes>
+      <div className="min-h-screen bg-gray-100">
+        <header className="flex justify-between items-center p-4 bg-slate-900">
+          <h2 className="font-medium text-slate-100">Lista de Pedidos TMB</h2>
+        </header>
+
+        <div className="p-4">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <OrdersTable
+                  orders={orders}
+                  loader={loader}
+                  setOrders={setOrders}
+                />
+              }
+            />
+            <Route
+              path="/order/:id"
+              element={<OrderDetail orders={orders} />}
+            />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
